783de57621023ad77491d61bcf341390ac0613de
RainPeach <1213562100@qq.com>
2025-05-29 09:48:37 +0800

    Updates

---
+++ assets/cold-67X_JjJs.js
@@ -0,0 +1,1 @@
+const e="/restaurant/",a=[{name:"皮蛋豆腐",description:"冰镇嫩脂 流心皮蛋",price:12,image:`${e}images/cold/Q_tofu.webp`},{name:"口水鸡",description:"红油沁骨 麻香透肉",price:19,image:`${e}images/cold/saliva_chicken.webp`},{name:"酱牛肉",description:"筋肉琥珀 老卤透骨",price:38,image:`${e}images/cold/sauce_beef.webp`}];export{a as default};
